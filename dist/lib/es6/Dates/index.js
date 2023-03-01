8f46d64c79119272b99c7418aa80320b6a427314
chaim malov <chaimalov@gmail.com>
2023-03-01 21:03:39 +0200

    feat(): initial setup

---
+++ dist/lib/es6/Dates/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@hebcal/core");
+const days = core_1.HDate.daysInMonth(1, 5773);
+console.log(days);
+//# sourceMappingURL=index.js.map
